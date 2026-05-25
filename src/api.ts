a982054c1cc3e2645b1c487a9fc6846e83f75369
Chrys Penalber <106619860+chryspenalber@users.noreply.github.com>
2026-05-25 19:12:21 +0100

    atualiza crud

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Book, NewBook } from './types';
 
 // Atualize esta URL com o seu endpoint do crudcrud.com
-export const API_URL = 'https://crudcrud.com/api/ae7c19f7cb7e42338aa56f9a93ec4fb5/livros';
+export const API_URL = 'https://crudcrud.com/api/f8df8013983542afb7d27fe403526558/livros';
 
 export async function fetchBooks(): Promise<Book[]> {
   const response = await axios.get<Book[]>(API_URL);
